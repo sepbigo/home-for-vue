b422d94a65df23455429b03dbdba68f7b13efd00
鳌拜拜 <sep@aya.yale.edu>
2024-12-24 14:24:17 +0800

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,40 +1,40 @@
 export const siteConfig = {
   // 基本信息
-  name: "Handsome", // 作者名称
-  title: "Java后端开发工程师", // 职位头衔
-  siteName: "Handsome | Java后端开发工程师", // 网站标题
+  name: "团子", // 作者名称
+  title: "行走在红尘中的凡夫俗子", // 职位头衔
+  siteName: "团子 | 一个凡夫俗子", // 网站标题
   siteDescription:
-    "专注于Java、Spring Boot、微服务等后端技术开发的个人作品集网站", // 网站描述
-  siteKeywords: "Java开发,Spring Boot,微服务,后端开发,个人作品集,技术博客", // SEO关键词
+    "闲话终日有，不听自然无", // 网站描述
+  siteKeywords: "摄影师,设计师,玩家,钢琴爱好者,影视后期,俗人", // SEO关键词
   author: "Handsome", // 作者信息
 
   // 图片资源配置
   images: {
     logo: "https://www.mmm.sd/upload/logo.png", // 网站Logo
-    icon: "https://www.mmm.sd/upload/logo.png", // 网站图标
-    avatar: "https://www.mmm.sd/upload/logo.png", // 个人头像
+    icon: "https://studio.ikun.su/d/BQACAgEAAxkDAAPLZ2pTdAxOc-iejTw68H0-ks4plZsAAjQEAALFc1FHFeyzanQf8WM2BA", // 网站图标
+    avatar: "https://studio.ikun.su/d/BAACAgEAAxkDAAPKZ2pTDtYng0vPCj83dT7Zad__8qQAAjMEAALFc1FHwPTBLP2IbEo2BA", // 个人头像
     ogImage: "https://www.mmm.sd/upload/logo.png", // 社交分享图片
   },
 
   // 个性化配置
-  slogan: "生活原本沉闷，但跑起来就有风。", // 个性签名
-  skills: ["Java", "Spring Boot", "MySQL", "Redis", "Docker", "Git"], // 技能标签
+  slogan: "闲话终日有，不听自然无。", // 个性签名
+  skills: ["摄影", "设计", "表演", "钢琴", "影视后期", "音乐"], // 技能标签
 
   // SEO 相关配置
   language: "zh-CN", // 网站语言
   themeColor: "#4F46E5", // 主题色
-  twitterHandle: "@hanmdsomeX", // Twitter账号
-  githubHandle: "acanyo", // GitHub账号
+  twitterHandle: "", // Twitter账号
+  githubHandle: "sepbigo", // GitHub账号
 
   // Schema.org 结构化数据
   organization: {
-    name: "Handsome's Portfolio", // 组织名称
+    name: "团子艺术工作室", // 组织名称
     logo: "https://www.mmm.sd/upload/logo.png", // 组织Logo
   },
 
   // 社交媒体链接
   social: {
-    github: "https://github.com/acanyo", // GitHub主页
-    email: "30819792@qq.com", // 联系邮箱
+    github: "https://github.com/sepbigo", // GitHub主页
+    email: "sep@aya.yale.edu", // 联系邮箱
   },
 };
